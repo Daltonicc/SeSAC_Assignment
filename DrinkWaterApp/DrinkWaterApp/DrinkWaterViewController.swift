04a8cb1df62d47a27cbf5e45575abed58dc12ff1
EthanPark <ethanpark604@gmail.com>
2021-10-08 22:50:11 +0900

    Bar button item 세부 UI 완성

--- DrinkWaterApp/DrinkWaterApp/DrinkWaterViewController.swift
+++ DrinkWaterApp/DrinkWaterApp/DrinkWaterViewController.swift
@@ -5,25 +5,45 @@
 //  Created by 박근보 on 2021/10/08.
 //
 
+//네비게이션 바 아래 보더라인 생성 어떻게?
+
 import UIKit
 
 class DrinkWaterViewController: UIViewController {
 
+    @IBOutlet var removeButton: UIBarButtonItem!
+    @IBOutlet var profileButton: UIBarButtonItem!
+    @IBOutlet var topLabel: UILabel!
+    @IBOutlet var middleLabel: UILabel!
+    @IBOutlet var bottomLabel: UILabel!
+    @IBOutlet var cactusImageView: UIImageView!
+    @IBOutlet var muchWaterLabel: UILabel!
+    @IBOutlet var haveToDrinkLabel: UILabel!
+    @IBOutlet var drinkingButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(displayP3Red: 75/255, green: 157/255, blue: 125/255, alpha: 1.0)
+        
+        //네비게이션 바 버튼 아이템 이미지 커스텀뷰
+        //이미지 사이즈 좀만 더 크게 할 순 없나..
+        let removebuttonImage = UIButton(type: .custom)
+        removebuttonImage.setImage(UIImage(systemName: "arrow.clockwise"), for: .normal)
+        removebuttonImage.tintColor = .white
+        
+        let profileButtonImage = UIButton(type: .custom)
+        profileButtonImage.setImage(UIImage(systemName: "person.circle"), for: .normal)
+        profileButtonImage.tintColor = .white
 
-        // Do any additional setup after loading the view.
+        removeButton.customView = removebuttonImage
+        profileButton.customView = profileButtonImage
+        
+        
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
