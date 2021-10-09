45a96e53b8f420a9e6d6abea6709583892664a81
EthanPark <ethanpark604@gmail.com>
2021-10-09 23:49:37 +0900

    프로필 화면 UI구현 완료

--- DrinkWaterApp/DrinkWaterApp/DrinkWaterViewController.swift
+++ DrinkWaterApp/DrinkWaterApp/DrinkWaterViewController.swift
@@ -17,6 +17,7 @@ class DrinkWaterViewController: UIViewController {
     @IBOutlet var middleLabel: UILabel!
     @IBOutlet var bottomLabel: UILabel!
     @IBOutlet var cactusImageView: UIImageView!
+    @IBOutlet var waterTextField: UITextField!
     @IBOutlet var muchWaterLabel: UILabel!
     @IBOutlet var haveToDrinkLabel: UILabel!
     @IBOutlet var drinkingButton: UIButton!
@@ -29,19 +30,26 @@ class DrinkWaterViewController: UIViewController {
         
         //네비게이션 바 버튼 아이템 이미지 커스텀뷰
         //이미지 사이즈 좀만 더 크게 할 순 없나..
-        let removebuttonImage = UIButton(type: .custom)
-        removebuttonImage.setImage(UIImage(systemName: "arrow.clockwise"), for: .normal)
-        removebuttonImage.tintColor = .white
-        removebuttonImage.addTarget(self, action: #selector(DrinkWaterViewController.viewDidLoad), for: .touchUpInside)
+        //스토리보드에 바 버튼 아이템을 가져와서 넣어야 하는데 일반 버튼 넣어놓고 왜 안되냐고 하고있었음. 진짜 멍청했다. 그래도 코드로 하는 법 알았으니 만족,,
         
-        let profileButtonImage = UIButton(type: .custom)
-        profileButtonImage.setImage(UIImage(systemName: "person.circle"), for: .normal)
-        profileButtonImage.tintColor = .white
-        profileButtonImage.addTarget(self, action: #selector(profiletapButton), for: .touchUpInside)
+//        let removebuttonImage = UIButton(type: .custom)
+//        removebuttonImage.setImage(UIImage(systemName: "arrow.clockwise"), for: .normal)
+//        removebuttonImage.tintColor = .white
+//        removebuttonImage.addTarget(self, action: #selector(DrinkWaterViewController.viewDidLoad), for: .touchUpInside)
+//
+//        let profileButtonImage = UIButton(type: .custom)
+//        profileButtonImage.setImage(UIImage(systemName: "person.circle"), for: .normal)
+//        profileButtonImage.tintColor = .white
+//        profileButtonImage.addTarget(self, action: #selector(profiletapButton), for: .touchUpInside)
         
 
-        removeButton.customView = removebuttonImage
-        profileButton.customView = profileButtonImage
+//        removeButton.customView = removebuttonImage
+//        profileButton.customView = profileButtonImage
+        
+        //새로 짠 코드
+        removeButton.image = UIImage(systemName: "arrow.clockwise")
+        profileButton.image = UIImage(systemName: "person.circle")
+        
         
         //Label UI
         topLabel.text = "잘하셨어요!\n오늘 마신 양은"
@@ -53,22 +61,34 @@ class DrinkWaterViewController: UIViewController {
         cactusImageView.layer.cornerRadius = cactusImageView.bounds.width / 2
         cactusImageView.layer.masksToBounds = true
         
-        muchWaterLabel.textAlignment = .center
+        
+        waterTextField.backgroundColor = UIColor(displayP3Red: 75/255, green: 157/255, blue: 125/255, alpha: 1.0)
+        waterTextField.borderStyle = .none
+        waterTextField.textAlignment = .right
+        waterTextField.font = UIFont.systemFont(ofSize: 25)
+        waterTextField.textColor = .white
+        
+        muchWaterLabel.font = UIFont.systemFont(ofSize: 25)
+        muchWaterLabel.textColor = .white
+        muchWaterLabel.text = "ml"
         
         haveToDrinkLabel.textAlignment = .center
+        haveToDrinkLabel.textColor = .white
         
         drinkingButton.setTitle("물마시기", for: .normal)
         drinkingButton.tintColor = .black
         
+        
     }
     
-    @objc func profiletapButton() {
-        
-        // 인스턴스 타입을 전달해줘야 함. 요런 식으로
-        // let vc = ProfileViewController()
-        navigationController?.pushViewController(ProfileViewController(), animated: true)
-        
-    }
+    // 아까 위에 잘못 알았던 거 연장선
+//    @objc func profiletapButton() {
+//
+//        // 인스턴스 타입을 전달해줘야 함. 요런 식으로
+//         let vc = ProfileViewController()
+//        navigationController?.pushViewController(vc, animated: true)
+//
+//    }
 
 
 }
--- DrinkWaterApp/DrinkWaterApp/ProfileViewController.swift
+++ DrinkWaterApp/DrinkWaterApp/ProfileViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import TextFieldEffects
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet var cactustImageView2: UIImageView!
+    @IBOutlet var saveButton: UIBarButtonItem!
+    @IBOutlet var nicknameTextField: HoshiTextField!
+    @IBOutlet var heightTextField: HoshiTextField!
+    @IBOutlet var weightTextField: HoshiTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        view.backgroundColor = UIColor(displayP3Red: 75/255, green: 157/255, blue: 125/255, alpha: 1.0)
+        
+        saveButton.title = "저장"
+        
+        textFieldSetting(textFieldName: nicknameTextField)
+        textFieldSetting(textFieldName: heightTextField)
+        textFieldSetting(textFieldName: weightTextField)
+        
+        nicknameTextField.placeholder = "닉네임을 설정해주세요"
+        heightTextField.placeholder = "키(cm)를 설정해주세요"
+        weightTextField.placeholder = "몸무게(kg)를 설정해주세요"
+        
+        
     }
     
+    func textFieldSetting(textFieldName textfield: HoshiTextField) {
+        
+        textfield.borderStyle = .none
+        textfield.placeholderColor = .white
+        textfield.placeholderFontScale = 0.8
+        textfield.textColor = .white
+        textfield.font = UIFont.systemFont(ofSize: 20)
+        textfield.borderActiveColor = .white
+        textfield.borderInactiveColor = .white
+        
+        
+        
+        
+    }
 
 }
