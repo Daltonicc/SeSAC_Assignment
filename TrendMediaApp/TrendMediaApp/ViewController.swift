afd326d3608ab5b12c68d3ed1bf826b7a03f93c6
EthanPark <ethanpark604@gmail.com>
2021-10-16 12:51:19 +0900

    트렌드 미디어 앱 파일 추가

---
+++ TrendMediaApp/TrendMediaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrendMediaApp
+//
+//  Created by 박근보 on 2021/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
