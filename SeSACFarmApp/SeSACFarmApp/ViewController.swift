42b6d776b9170e0ed9b2245ad1f8f73b9bf86bae
EthanPark <ethanpark604@gmail.com>
2022-01-01 23:25:20 +0900

    SeSACFarmApp

---
+++ SeSACFarmApp/SeSACFarmApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeSACFarmApp
+//
+//  Created by 박근보 on 2022/01/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
+
---
+++ SeSACFramework/SeSACFramework/SeSAC.swift
@@ -0,0 +1,32 @@
+//
+//  SeSAC.swift
+//  SeSACFramework
+//
+//  Created by 박근보 on 2021/12/23.
+//
+
+import Foundation
+import UIKit
+
+open class SeSACOpen: UIViewController {
+    
+    var name: String = "고래밥"
+}
+
+public class SeSACPublic: UIViewController {
+    
+    var name: String = "고래밥"
+}
+
+internal class SeSACInternal: UIViewController {
+    
+}
+
+fileprivate class SeSACFilePrivate: UIViewController {
+    
+}
+
+private class SeSACPrivate: UIViewController {
+    
+}
+
